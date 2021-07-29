93c59dcaa56f86736427dfb6ca31fbcad03c2528
Samir Mansour <smyrmansour@gmail.com>
2021-07-30 00:10:44 +0300

    Custom annotation to access currently logged in user repaired

--- src/main/java/com/example/polls/security/CurentUser.java
+++ src/main/java/com/example/polls/security/CurentUser.java
@@ -3,7 +3,7 @@ package com.example.polls.security;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import java.lang.annotation.*;
 
-@Target({ElementType.PARAMETER, )
+@Target({ElementType.PARAMETER, ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @AuthenticationPrincipal
